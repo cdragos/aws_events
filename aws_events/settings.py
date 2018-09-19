8e946e458969c92b7aadee79e1f98db80767ba15
Dragos Catarahia <dragos@catarahia.me>
2018-09-19 23:49:52 +0300

    Use environment variables in settings.

--- aws_events/settings.py
+++ aws_events/settings.py
@@ -1,8 +1,18 @@
 from pathlib import Path
+import os
 
 
-KINESIS_STREAM = 'olxgroup-reservoir-recruit'
-KINESIS_RECORDS_LIMIT = 1000
+KINESIS_STREAM = os.environ.get('KINESIS_STREAM', 'olxgroup-reservoir-recruit')
+KINESIS_RECORDS_LIMIT = os.environ.get('KINESIS_RECORDS_LIMIT', 1000)
 
-DOWNLOAD_PATH = Path(__file__).parent / 'assets'
-RULES_PATH = Path(__file__).parent / 'rules.yml'
+if 'DOWNLOAD_PATH' in os.environ:
+    DOWNLOAD_PATH = Path(os.environ['DOWNLOAD_PATH'])
+else:
+    DOWNLOAD_PATH = Path(__file__).parent / 'assets'
+
+if 'RULES_PATH' in os.environ:
+    RULES_PATH = Path(os.environ['RULES_PATH'])
+else:
+    RULES_PATH = Path(__file__).parent / 'rules.yml'
+
+TRACK_FILE_URL = 'https://tracking-dev.onap.io/h/bdyt-case-ex1-dc'
